0569c100d7b9e6b5e1aa524c6dc589253c45a64d
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-01-05 22:01:46 +0200

    Initial Commit

---
+++ SwiftulFirebaseBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftulFirebaseBootcamp
+//
+//  Created by Toni Stoyanov on 5.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftulFirebaseBootcamp/SwiftulFirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulFirebaseBootcampApp.swift
+//  SwiftulFirebaseBootcamp
+//
+//  Created by Toni Stoyanov on 5.01.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftulFirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
