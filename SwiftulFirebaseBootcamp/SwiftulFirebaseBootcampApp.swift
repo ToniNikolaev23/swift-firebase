3a8013c9c0cbaa77197efc0250e7ad658ac1aa1d
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-01-05 22:26:52 +0200

    Init firebase

--- SwiftulFirebaseBootcamp/SwiftulFirebaseBootcampApp.swift
+++ SwiftulFirebaseBootcamp/SwiftulFirebaseBootcampApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct SwiftulFirebaseBootcampApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+        print("Configured Firebase")
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
