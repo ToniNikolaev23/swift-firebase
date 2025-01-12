52247532d682fbb945d002b5f7576cd895bd3845
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-01-12 15:06:56 +0200

    Sorting, filtering and querying firebase database data

--- SwiftulFirebaseBootcamp/Products/ProductsView.swift
+++ SwiftulFirebaseBootcamp/Products/ProductsView.swift
@@ -11,9 +11,58 @@ import SwiftUI
 final class ProductsViewModel: ObservableObject {
     
     @Published private(set) var products: [Product] = []
+    @Published var selectedFilter: FilterOption? = nil
+    @Published var selectedCategory: CategoryOption? = nil
     
-    func getAllProducts() async throws {
-        self.products = try await ProductsManager.shared.getAllProducts()
+//    func getAllProducts() async throws {
+//        self.products = try await ProductsManager.shared.getAllProducts()
+//    }
+    
+    enum FilterOption: String, CaseIterable {
+        case noFilter
+        case priceHigh
+        case priceLow
+        
+        var priceDescending: Bool? {
+            switch self {
+            case .noFilter: return nil
+            case .priceHigh: return true
+            case .priceLow: return false
+            }
+        }
+    }
+    
+    func filterSelected(option: FilterOption) async throws {
+        self.selectedFilter = option
+        self.getProducts()
+        
+    }
+    
+    enum CategoryOption: String, CaseIterable {
+        case noCategory
+        case smartphones
+        case laptops
+        case fragrances
+        
+        var categoryKey: String? {
+            if self == .noCategory {
+                return nil
+            }
+            
+            return self.rawValue
+        }
+    }
+    
+    func categorySelected(option: CategoryOption) async throws {
+        self.selectedCategory = option
+        self.getProducts()
+        
+    }
+    
+    func getProducts() {
+        Task {
+            self.products = try await ProductsManager.shared.getAllProducts(priceDescending: selectedFilter?.priceDescending, forCategory: selectedCategory?.categoryKey)
+        }
     }
 }
 
@@ -26,8 +75,37 @@ struct ProductsView: View {
             }
         }
         .navigationTitle("Products")
+        .toolbar(content: {
+            ToolbarItem(placement: .topBarLeading) {
+                Menu("Filter: \(viewModel.selectedFilter?.rawValue ?? "NONE")") {
+                    ForEach(ProductsViewModel.FilterOption.allCases, id: \.self) { filterOption in
+                        Button(action: {
+                            Task{
+                                try? await viewModel.filterSelected(option: filterOption)
+                            }
+                        }, label: {
+                            Text(filterOption.rawValue)
+                        })
+                    }
+                }
+            }
+            
+            ToolbarItem(placement: .topBarLeading) {
+                Menu("Category: \(viewModel.selectedCategory?.rawValue ?? "NONE")") {
+                    ForEach(ProductsViewModel.CategoryOption.allCases, id: \.self) { categoryOption in
+                        Button(action: {
+                            Task{
+                                try? await viewModel.categorySelected(option: categoryOption)
+                            }
+                        }, label: {
+                            Text(categoryOption.rawValue)
+                        })
+                    }
+                }
+            }
+        })
         .task {
-            try? await viewModel.getAllProducts()
+            viewModel.getProducts()
         }
     }
 }
